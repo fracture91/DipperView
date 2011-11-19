7b9368b16f1b68eb4c041cbd2f87959c8d7ee533
Andrew Hurle <fracture91@gmail.com>
2011-11-18 22:37:13 -0500

    Check for Ethernet headers.

--- dipperview.cpp
+++ dipperview.cpp
@@ -5,20 +5,25 @@ using namespace std;
 int main(int argc, char* argv[]) {
 	
 	pcap_t *capture;
-	char errbuf[PCAP_ERRBUF_SIZE];
+	char errorBuffer[PCAP_ERRBUF_SIZE];
+	int linkType;
 	
 	if(argc != 2) {
 		cerr << "Must provide one argument:  the filename of a tcpdump file" << endl;
 		return -1;
 	}
 
-	capture = pcap_open_offline(argv[1], errbuf);
+	capture = pcap_open_offline(argv[1], errorBuffer);
 	if(capture == NULL) {
-		cerr << "pcap_open_offline error: " << errbuf << endl;
+		cerr << "pcap_open_offline error: " << errorBuffer << endl;
 		return -1;
 	}
-
-	//Check that the data you are provided has been captured from Ethernet using function pcap_datalink().
+	
+	linkType = pcap_datalink(capture);
+	if(linkType != DLT_EN10MB) {
+		cerr << "Error: packet does not have Ethernet headers" << endl;
+		return -1;
+	}
 
 	//Read packets from the file using function pcap_loop().
     
